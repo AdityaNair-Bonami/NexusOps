896ccdcaf28d0cc46cb8973a1c503541304c1931
Aditya Nair <aditya.nair@bonamisoftware.com>
2026-02-18 11:39:53 +0530

    Implemented RBAC with JWT authentication

--- app/main.py
+++ app/main.py
@@ -7,12 +7,12 @@ app = FastAPI(title="NexusOps API")
 
 fake_users_db = {
     "jr_dev": {
-        "username:": "junior_dev",
+        "username": "junior_dev",
         "role": "junior_dev",
         "hashed_password": get_password_hash("jr_dev")
     },
     "sr_dev": {
-        "username:": "senior_dev",
+        "username": "senior_dev",
         "role": "senior_dev",
         "hashed_password": get_password_hash("sr_dev")
     }
@@ -21,16 +21,19 @@ fake_users_db = {
 @app.post("/token")
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
     user = fake_users_db.get(form_data.username)
+    
+
     if not user or not verify_password(form_data.password, user["hashed_password"]):
         raise HTTPException(status_code=400, detail="Incorrect username or password")
     
-    # issue token with the role
-    access_token = create_access_token(data={"sub":user["username"], "role": user["role"]})
-    return {"access_token":access_token, "token_type":"beared"}
+    # Issue token with the role
+    access_token = create_access_token(data={"sub": user["username"], "role": user["role"]})
+    
+    return {"access_token": access_token, "token_type": "bearer"}
 
 @app.get("/junior/tasks", dependencies=[Depends(RoleChecker(["junior_dev", "senior_dev"]))])
 async def read_tasks():
-    return {"message": "You can see this because you are atleast a junior developer."}
+    return {"message": "You can see this because you are at least a junior developer."}
 
 @app.get("/senior/deploy", dependencies=[Depends(RoleChecker(["senior_dev"]))])
 async def deploy_code():
